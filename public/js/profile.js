71edbfeeb792acace70f576160c1d4861fe28c03
MKazmTECHNI <u123_mackaz_waw@technischools.com>
2025-01-16 15:11:07 +0100

    forgor to add changing profile logic to actual file

--- public/js/profile.js
+++ public/js/profile.js
@@ -16,6 +16,39 @@ function convertToImage(base64String1) {
   }
 }
 
+async function handleChangeProfilePicture(sztring) {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser")); // Parse the JSON object
+  const token = currentUser?.token;
+  const userId = currentUser?.id;
+
+  if (!token) {
+    alert("You are not authorized. Please log in first.");
+    return;
+  }
+  try {
+    const response = await fetch(`${server_adress}/changeProfile`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Attach the token in the Authorization header
+      },
+      body: JSON.stringify({
+        profilePicBase64String: sztring,
+        user_id: userId,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      alert(`Error: ${errorData.message}`);
+      return;
+    }
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    alert("Failed to add comment");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   let currentUserId;
   let currentUserName;
@@ -91,18 +124,27 @@ document.addEventListener("DOMContentLoaded", async () => {
   const desiredWidth = 200; // Desired width of the cropped image
   const desiredHeight = 200; // Desired height of the cropped image
 
-  profilePictureImg.addEventListener("click", function (event) {
-    fileInput.click();
-  });
+  profilePictureImg.addEventListener("click", fileInput.click());
   fileInput.addEventListener("change", function () {
-    const file = fileInput.files[0];
+    const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
-
-      reader.onload = function (e) {
-        profilePictureImg.src = e.target.result;
+      reader.onload = () => {
+        const img = new Image();
+        img.onload = () => {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+          const [maxW, maxH] = [280, 140];
+          let [w, h] = [img.width, img.height];
+          if (w > maxW || h > maxH)
+            [w, h] = w > h ? [maxW, (h * maxW) / w] : [(w * maxH) / h, maxH];
+          canvas.width = w;
+          canvas.height = h;
+          ctx.drawImage(img, 0, 0, w, h);
+          convertToImage(canvas.toDataURL("image/jpeg", 0.2).split(",")[1]);
+        };
+        img.src = reader.result;
       };
-
       reader.readAsDataURL(file);
     }
   });
